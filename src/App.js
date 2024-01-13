f8f373820f2900a5ce4229dc5fe13e1ab0543668
Daleray1231 <daleray1231@gmail.com>
2024-01-13 13:23:59 -0600

    installed node modules

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
