03bcad960390d504e8edeb4fc88f405045c977ca
Daleray1231 <daleray1231@gmail.com>
2024-01-13 17:21:46 -0600

    removed unused files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          {/* <CodeIcon className="mx-auto inline-block w-10 mb-4" /> */}
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
