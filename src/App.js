7942e90f2c7ba9d7b0d7b6264c92868ad15499e2
Daleray1231 <daleray1231@gmail.com>
2024-01-13 12:44:45 -0600

    added components and file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
