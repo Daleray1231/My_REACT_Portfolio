c88f5b63737dcfefe66e6580b015c9457ffe5d65
Daleray1231 <daleray1231@gmail.com>
2024-01-13 12:48:30 -0600

    added import components to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
