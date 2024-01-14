f36a0c11672d5ffd63a9a70d8f2c766e520fbd4b
Daleray1231 <daleray1231@gmail.com>
2024-01-13 19:53:22 -0600

    image changein contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,16 +28,11 @@ export default function Contact() {
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        title="map"
-                        className="absolute inset-0"
-                        frameBorder={0}
-                        marginHeight={0}
-                        marginWidth={0}
-                        style={{ filter: "opacity(0.7)" }}
+                    <img
                         src="./suit_finn.jpg"
+                        alt="Suit Finn"
+                        className="w-full h-full object-cover object-center absolute inset-0"
+                        style={{ filter: "opacity(0.7)" }}
                     />
                     <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
                         <div className="lg:w-1/2 px-6">
