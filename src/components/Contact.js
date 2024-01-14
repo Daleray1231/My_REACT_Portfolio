5eb15b21edda77b1aa3e8729791ea68bf1709297
Daleray1231 <daleray1231@gmail.com>
2024-01-14 17:05:14 -0600

    changes to handleSubmit function, testing for user input in Forms/Netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,19 +14,19 @@ export default function Contact() {
     }
     function handleSubmit(e) {
         e.preventDefault();
-
+    
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: encode({ "form-name": "contact", name, email, message }),
         })
-            .then(() => {
-                alert("Message sent!");
-
-                // Reset form fields using the form's reset method
-                e.target.reset();
-            })
-            .catch((error) => alert(error));
+        .then(() => {
+            alert("Message sent!");
+            setName("");
+            setEmail("");
+            setMessage("");
+        })
+        .catch((error) => alert(error));
     }
 
     return (
