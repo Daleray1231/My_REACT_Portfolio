7019dda048f3af10682840e6245c65c5ff971671
Daleray1231 <daleray1231@gmail.com>
2024-01-14 10:16:24 -0600

    added map/made style changes in contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,13 +28,14 @@ export default function Contact() {
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    <img
-                        src="./funny_finn.jpg"
-                        alt="Suit Finn"
-                        className="w-full h-full object-cover object-center absolute inset-0"
-                        style={{ filter: "opacity(0.7)" }}
-                    />
-                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443092.71647142264!2d-96.02442638173717!3d29.816312181113098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX!5e0!3m2!1sen!2sus!4v1705248666160!5m2!1sen!2sus"/>
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md" style={{ width: '750px' }}>
                         <div className="lg:w-1/2 px-6">
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 ADDRESS
@@ -63,10 +64,10 @@ export default function Contact() {
                     name="contact"
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                        Hire Me
+                        Let's work together!
                     </h2>
                     <p className="leading-relaxed mb-5">
-                        This is why you should hire me.
+                       {/* Text */}
                     </p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
