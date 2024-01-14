4e294bc385972fa109bdce4d586931c13643188a
Daleray1231 <daleray1231@gmail.com>
2024-01-13 18:32:46 -0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Testimonials from "./components/Testimonials";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      {/* <Navbar /> */}
+      <Navbar />
       <About />
       <Projects />
       <Skills />
