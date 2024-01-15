4aaac0f05d11ebe06e1a70bd0ff521b253ee4431
Daleray1231 <daleray1231@gmail.com>
2024-01-15 13:09:02 -0600

    update to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -75,7 +75,7 @@ export default function Contact() {
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
                 >
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                        Hire Me
+                        Let's work together!
                     </h2>
                     <p className="leading-relaxed mb-5">
                     </p>
