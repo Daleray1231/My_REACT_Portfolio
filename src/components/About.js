e8230013bd4d0547dca69283333ae6a306c640ff
Daleray1231 <daleray1231@gmail.com>
2024-01-16 19:13:12 -0600

    added hidden effect

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,17 @@ import React, { useEffect, useRef } from "react";
 
 export default function About() {
   const headingRef = useRef(null);
+  const audioRef = useRef(null);
 
   useEffect(() => {
     headingRef.current.focus();
   }, []);
+
+  const handleImageClick = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
 
   return (
     <section id="about">
@@ -15,34 +22,40 @@ export default function About() {
             ref={headingRef}
             tabIndex="-1"
             className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white focus:outline-none"
-            style={{ outline: "none" }} // This inline style removes the focus outline
+            style={{ outline: "none" }}
           >
             Hi, I'm Dale.
             <br className="hidden lg:inline-block" />
           </h1>
-          <p className="mb-8 leading-relaxed" style={{ textIndent: '1em' }}>I am an aspiring developer ready to make my mark in the tech industry. Currently my expertise lies in front end web applications or as Full Stack development team member. I'm constantly researching new tech and starting new projects to get as much exposure as I can. My tuned ambition and perseverance help me to always maintain my vision and reach my desired goals. Coming to you with over a decade in customer service skills, and a variety of technical skills, I believe I would be a great asset to any development team.
+          <p className="mb-8 leading-relaxed" style={{ textIndent: '1em' }}>
+            I am an aspiring developer ready to make my mark in the tech industry. Currently, my expertise lies in front end web applications or as a Full Stack development team member. I'm constantly researching new tech and starting new projects to get as much exposure as I can. My tuned ambition and perseverance help me to always maintain my vision and reach my desired goals. Coming to you with over a decade in customer service skills, and a variety of technical skills, I believe I would be a great asset to any development team.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              style={{ cursor: "pointer" }} // Set cursor to pointer on hover
             >
               Work With Me
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              style={{ cursor: "pointer" }} // Set cursor to pointer on hover
             >
               See My Past Work
             </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <audio ref={audioRef} src="./Recording.m4a"></audio>
+
           <img
             className="object-cover object-center rounded"
             alt="hero"
             src="./funny_finn.jpg"
-            style={{ borderRadius: '35%' }}
+            style={{ borderRadius: '35%', cursor: "pointer" }} // Set cursor to pointer on hover
+            onClick={handleImageClick}
           />
         </div>
       </div>
