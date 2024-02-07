15791e6f1c63f1669439ac78bfd217d8f191bc89
Daleray1231 <daleray1231@gmail.com>
2024-02-07 14:00:44 -0600

    build successful

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +6,8 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  content: [
+    './src/index.css',
+    './src/Projects.css',
+  ],
 }
