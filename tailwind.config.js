16860809544d0810007e92db7b751fcba8c1b6a5
Daleray1231 <daleray1231@gmail.com>
2024-02-07 14:06:33 -0600

    added files to tailwind.config content

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,14 @@ module.exports = {
   content: [
     './src/index.css',
     './src/Projects.css',
+    './src/index.jsx',
+    './src/index.css',
+    './src/About.jsx',
+    './src/Contact.jsx',
+    './src/Footer.jsx',
+    './src/Navbar.jsx',
+    './src/Projects.jsx',
+    './src/Skills.jsx',
+    './src/Testimonials.jsx'
   ],
 }
